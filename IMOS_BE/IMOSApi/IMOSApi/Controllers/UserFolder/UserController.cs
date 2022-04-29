cbb93f584e8e9d9f97e55e2ad9985748fc40c417
ChaseNel <62016103+ChaseNel@users.noreply.github.com>
2022-04-29 15:08:35 +0200

    Login v03

---
+++ IMOS_BE/IMOSApi/IMOSApi/Controllers/UserFolder/UserController.cs
@@ -0,0 +1,41 @@
+﻿using IMOSApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMOSApi.Controllers.UserFolder
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetUsers")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult Create([FromBody] User user
+            )
+        {
+            return Ok();
+        }
+
+        [HttpPut("UpdateUser")]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+
+        [HttpDelete("DeleteUser")]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
