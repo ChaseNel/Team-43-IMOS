cc016e05db86646feb7bd5dc318892a4c4d7b702
Josh Da Silva <u17047162@tuks.co.za>
2022-05-01 22:38:30 +0200

    Some Controllers and other skeleton

---
+++ IMOS_BE/IMOSApi/IMOSApi/Controllers/DocumentController - Copy (5).cs
@@ -0,0 +1,64 @@
+﻿using IMOSApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMOSApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DocumentController : ControllerBase
+    {
+        [HttpGet("GetDocuments")]
+        public IEnumerable<Document> Retrieve()
+        {
+            using (var context = new IMOSContext())
+            {
+                return context.Documents.ToList();
+            }
+        }
+        [HttpGet("GetDocument/{id}")]
+        public IEnumerable<Document> Get(int id)
+        {
+            using (var context = new IMOSContext())
+            {
+                IEnumerable<Document> tmp = context.Documents.Where(emp => emp.DocumentId == id).ToList();
+                return tmp;
+            }
+        }
+        [HttpPost("CreateDocument")]
+        public IActionResult Create([FromBody] Document Document)
+        {
+            using (var context = new IMOSContext())
+            {
+                context.Documents.Add(Document);
+                context.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpPut("UpdateDocument/{Id}")]
+        public void Update([FromBody] Document Document, [FromRoute] int Id)
+        {
+            using (var context = new IMOSContext())
+            {
+                var clie = context.Documents.Where(clie => clie.DocumentId == Id).ToList().FirstOrDefault();
+                //emp.
+                context.SaveChanges();
+            }
+        }
+        [HttpDelete("DeleteEmployee/{Id}")]
+        public void Delete(int id)
+        {
+            using (var context = new IMOSContext())
+            {
+                var clie = context.Documents.Where(clie => clie.DocumentId == id).ToList().FirstOrDefault(); ;
+                context.Documents.Remove(clie);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Imos/src/app/app.module.ts
+++ Imos/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
