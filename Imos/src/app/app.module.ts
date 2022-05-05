5aecf576c153ef42aa14bc453f41b94c99e0129c
u17047162 <63853705+u17047162@users.noreply.github.com>
2022-05-05 14:55:43 +0200

    UploadAttempt

--- Imos/src/app/app.module.ts
+++ Imos/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatTableModule} from '@angular/material/table';
 
+//Angular Material Files
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+//
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,12 +23,17 @@ import { UserRoleComponent } from './user-role/user-role.component';
     EmployeeComponent,
     HomeComponent,
     UserComponent,
-    UserRoleComponent
+    UserRoleComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
