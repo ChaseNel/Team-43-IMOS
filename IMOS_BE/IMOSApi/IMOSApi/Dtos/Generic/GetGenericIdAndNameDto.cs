9f46cb772983e56ec448c3a479e08b38ceff3235
ngcebo <u18180559@tuks.co.za>
2022-06-21 23:50:49 +0200

    SafetyFileItem

--- IMOS_BE/IMOSApi/IMOSApi/Dtos/Generic/GetGenericIdAndNameDto.cs
+++ IMOS_BE/IMOSApi/IMOSApi/Dtos/Generic/GetGenericIdAndNameDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace IMOSApi.Dtos.Generic
     public class GetGenericIdAndNameDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
     }
